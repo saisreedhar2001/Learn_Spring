65c86e768a17498392655b8106600ee2e5513dd0
sai sreedhar <saisreedhar2001@gmail.com>
2024-06-17 23:27:28 +0530

    added feature to get questions list

--- quizapp/src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ quizapp/src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -1,14 +1,20 @@
 package com.telusko.quizapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.telusko.quizapp.model.QuestionTableWrapper;
+import com.telusko.quizapp.model.questiontable;
 import com.telusko.quizapp.service.QuizService;
 
 @RestController
@@ -21,6 +27,11 @@ public class QuizController {
 	@PostMapping("create")
 	public ResponseEntity<String> createQuiz(@RequestParam String Category,@RequestParam int noQ,@RequestParam String Title) {
 		return quizService.createQuiz(Category,noQ,Title);
+	}
+	
+	@GetMapping("get/{id}")
+	public ResponseEntity<List<QuestionTableWrapper>> getQuizQuestions(@PathVariable Integer id){
+		return quizService.getQuizQuestions(id);
 	}
 
 }
--- quizapp/src/main/java/com/telusko/quizapp/service/QuizService.java
+++ quizapp/src/main/java/com/telusko/quizapp/service/QuizService.java
@@ -1,6 +1,8 @@
 package com.telusko.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.telusko.quizapp.dao.QuestionDAO;
 import com.telusko.quizapp.dao.QuizDAO;
+import com.telusko.quizapp.model.QuestionTableWrapper;
 import com.telusko.quizapp.model.Quiz;
 import com.telusko.quizapp.model.questiontable;
 
@@ -31,6 +34,20 @@ public class QuizService {
 		quizDAO.save(quiz);
 		
 		return new ResponseEntity<>("Success",HttpStatus.CREATED);
+	}
+
+
+	public ResponseEntity<List<QuestionTableWrapper>> getQuizQuestions(Integer id) {
+		// TODO Auto-generated method stub
+		Optional<Quiz> quiz = quizDAO.findById(id);
+		List<questiontable> questionsFromDB= quiz.get().getQuestions();
+		List<QuestionTableWrapper> questionsForUser = new ArrayList<>();
+		
+		for(questiontable q : questionsFromDB) {
+			QuestionTableWrapper qWrapper = new QuestionTableWrapper(q.getId(), q.getQuestion_title(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+			questionsForUser.add(qWrapper);
+		}
+		return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
 	}
 
 }
